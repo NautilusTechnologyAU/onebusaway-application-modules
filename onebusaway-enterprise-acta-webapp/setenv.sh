e9664a2d5d3fe37a197b72913333ad59088d8a52
Nautilus Technology <saroar.jahan@nautilus-tech.com.au>
2022-11-04 17:33:07 +1030

    improvement: Standardising Dockerfile and scripts

--- onebusaway-enterprise-acta-webapp/setenv.sh
+++ onebusaway-enterprise-acta-webapp/setenv.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-sed -i 's@${AZURE_COGNITIVE_SERVICE_URL}@'"$AZURE_COGNITIVE_SERVICE_URL"'@g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
 sed -i 's@${TRANSIT_DATA_SERVICE_URL}@'"$TRANSIT_DATA_SERVICE_URL"'@g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
+sed -i 's/${GOOGLE_MAPS_API_KEY}/'"$GOOGLE_MAPS_API_KEY"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
 sed -i 's/${DATABASE_HOST}/'"$DATABASE_HOST"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
 sed -i 's/${DATABASE_PORT}/'"$DATABASE_PORT"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
 sed -i 's/${DATABASE_NAME}/'"$DATABASE_NAME"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
 sed -i 's/${DATABASE_USER}/'"$DATABASE_USER"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
 sed -i 's/${DATABASE_PASSWORD}/'"$DATABASE_PASSWORD"'/g' ${APP_PATH}/WEB-INF/classes/data-sources.xml
+
+sed -i 's/${GOOGLE_MAPS_API_KEY}/'"$GOOGLE_MAPS_API_KEY"'/g' /oba/config.json
+
